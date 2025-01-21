15ba1e8529ba779f9cdb9bcad29817f76c36c423
suvarna1405 <suvarna7404@gmail.com>
2025-01-21 22:37:17 +0530

    fibonacii

--- fibonacii.js
+++ fibonacii.js
@@ -1,3 +1,9 @@
 function fibonacii(n){
-    
+const fib=[0,1]
+ for(i=2;i<n;i++){
+    fib[i]=fib[i-1]+fib[i-2]
+ }return fib;
 }
+console.log(fibonacii(3))
+console.log(fibonacii(6))
+console.log(fibonacii(7))
